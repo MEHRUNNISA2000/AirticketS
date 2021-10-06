825683fe8a1973cf1c13ca95e55ce2e8c4c5b752
MEHRUNNISA2000 <91881771+MEHRUNNISA2000@users.noreply.github.com>
2021-10-06 14:51:17 +0530

    First commit

---
+++ AirlineReservation/src/air/Database.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package air;
+
+import java.sql.*;
+
+/**
+ *
+ * @author NVT
+ */
+public class Database {
+    
+    public Connection con;
+    
+    public Database(){
+     
+        
+        try{
+        
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+    con=DriverManager.getConnection("jdbc:mysql://localhost:3306/airticket","root","password");
+            System.out.println("connected to database successfully");
+        }catch(Exception e){
+            System.out.println("cannot connect to database"+e);
+        }
+    
+    }
+}
